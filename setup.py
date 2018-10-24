9f7ce54719d98a18508dae3b3d21f4fc880a61ab
Dennis Mannhart <dennis.mannhart@gmail.com>
2018-10-24 10:36:25 +0200

    new version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="uloganalysis",
-    version="0.0.1",
+    version="0.0.3",
     author="Dennis Mannhart",
     author_email="dennis.mannhart@gmail.com",
     description="A package to analys ulog files using pandas",
@@ -15,7 +15,6 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: BSD 3-Clause",
         "Operating System :: OS Independent",
     ],
 )
