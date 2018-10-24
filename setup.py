7ad46e71cc442b8bac0edacaedc628acbc7719db
Dennis Mannhart <dennis.mannhart@gmail.com>
2018-10-24 12:26:41 +0200

    update setup

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import setuptools
 
 with open("README.md", "r") as fh:
@@ -5,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="uloganalysis",
-    version="0.0.3",
+    version="0.0.4",
     author="Dennis Mannhart",
     author_email="dennis.mannhart@gmail.com",
     description="A package to analys ulog files using pandas",
@@ -13,8 +14,15 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/YUNEEC/ulogAnalysis",
     packages=setuptools.find_packages(),
+    install_requires=[
+        'matplotlib',
+        'numpy',
+        'pandas',
+        'pyulog',
+        'transforms3d'],
     classifiers=[
         "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
     ],
 )
