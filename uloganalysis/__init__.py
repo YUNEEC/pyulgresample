0695e0a1d99ab572ead4ba106bb1ff39364823fc
Dennis Mannhart <dennis.mannhart@gmail.com>
2018-10-29 17:18:44 +0100

    __init__ add docstring and version number

--- uloganalysis/__init__.py
+++ uloganalysis/__init__.py
@@ -1,1 +1,7 @@
-name="uloganalysis"
+"""
+uloganalysis
+A package that resamples ulog topics into one pandas dataframe. 
+The package also contains convenient functions to exctact addional information
+from the ulog topics. 
+"""
+__version__ = "0.0.1"
