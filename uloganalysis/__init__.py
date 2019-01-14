1edd32a51061c130030ef0eb4814f75101fd932a
Dennis Mannhart <dennis.mannhart@gmail.com>
2019-01-14 10:58:38 +0100

    rename folder uloganalysis to pyulgresample

--- uloganalysis/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-uloganalysis
-A package that resamples ulog topics into one pandas dataframe. 
-The package also contains convenient functions to exctact addional information
-from the ulog topics. 
-"""
-__version__ = "0.0.1"
