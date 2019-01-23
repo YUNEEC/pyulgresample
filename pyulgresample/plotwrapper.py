0cfd85b262b9768fd077db0cf2e6e75e2cc29f1f
PhiAbs <philipp@yuneecresearch.com>
2019-01-23 12:23:20 +0100

    continued: formatted comments such that they comply with pep257

--- pyulgresample/plotwrapper.py
+++ pyulgresample/plotwrapper.py
@@ -1,10 +1,15 @@
-"""
-wrapper for plotting
-"""
+"""wrapper for plotting."""
 import numpy as np
 
 
 def plot_time_series(df, plt):
+    """Plot a time series.
+    
+    Arguments:
+    df -- dataframe containing messages from the required topics
+    plt -- plot
+
+    """
     # Remove the plot frame lines
     delta = (df["timestamp"].max() - df["timestamp"].min()) / 10
     plt.xticks(
