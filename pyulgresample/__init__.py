bddcbe44f9a0c70aabedc18f91d4fc8e0c6c5c84
PhiAbs <philipp@yuneecresearch.com>
2019-01-23 13:27:16 +0100

    pep257 convention applied for all python files

--- pyulgresample/__init__.py
+++ pyulgresample/__init__.py
@@ -1,7 +1,8 @@
-"""
-pyulgresample
+"""pyulgresample.
+
 A package that resamples ulog topics into one pandas dataframe.
 The package also contains convenient functions to exctact addional information
 from the ulog topics.
+
 """
 __version__ = "0.0.1"
